8911dd8ca28fcc326fab1431d3269361f35c3f9f
Debaprasad <Debaprasad@192.168.0.14>
2019-03-12 01:00:14 +0530

    Adding system.ouy.println stms.

---
+++ 03_spring_mvc_custom_config/src/main/java/com/st/spring/boot/Application.java
@@ -0,0 +1,13 @@
+package com.st.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Git HUb Testing");
+		SpringApplication.run(Application.class, args);
+	}
+}
